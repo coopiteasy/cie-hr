b737835209fdce038297cf0a3b58c1820f7d126f
hugues de keyzer <odoo@hugues.info>
2026-01-27 14:55:37 +0100

    [ADD] hr_work_entry_contract_constrained_attendance
    add new module hr_work_entry_contract_constrained_attendance.

---
+++ hr_work_entry_contract_constrained_attendance/models/res_company.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    def _get_default_absence_hr_work_entry_type(self):
+        return self.env.ref("hr_work_entry_contract.work_entry_type_unpaid_leave")
+
+    absence_hr_work_entry_type_id = fields.Many2one(
+        "hr.work.entry.type",
+        "Absence Work Entry Type",
+        default=_get_default_absence_hr_work_entry_type,
+    )
