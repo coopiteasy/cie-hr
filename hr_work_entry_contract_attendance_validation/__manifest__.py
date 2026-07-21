9e656ea56fc79efbc074881cab34a796f8c31d61
hugues de keyzer <odoo@hugues.info>
2026-07-21 16:59:29 +0200

    [ADD] add new module
    add new module hr_work_entry_contract_attendance_validation.

---
+++ hr_work_entry_contract_attendance_validation/__manifest__.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Attendance Work Entries Generation Validation",
+    "summary": "Take validated work entries into account when generating work entries",
+    "version": "18.0.1.0.0",
+    "category": "Human Resources/Employees",
+    "website": "https://github.com/coopiteasy/cie-hr",
+    "author": "Coop IT Easy SC",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_work_entry_contract_attendance_oca",
+        "hr_work_entry_validation",
+    ],
+    "auto_install": True,
+}
---
+++ hr_work_entry_contract_attendance_validation/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+Take validated work entries into account when generating work entries.
+
+This is a glue module between `hr_work_entry_contract_attendance_oca` and `hr_work_entry_validation`.
